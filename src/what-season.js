24a3bd9618c8b0631f0a25e780c85d06e3b10925
Olga <70957483+Lola-1992@users.noreply.github.com>
2020-10-21 22:04:37 +0300

    fix:
    fix: change function

--- src/what-season.js
+++ src/what-season.js
@@ -1,22 +1,9 @@
-
 module.exports = function getSeason(date) {
-  if (date === null) {
-		return 'Unable to determine the time of year!';
-	} else if (date && Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date)) {
-		if (date.getMonth() <= 1 || date.getMonth() == 11) {
-			return 'winter';
-		}
-		else if (date.getMonth() <= 4) {
-			return 'spring';
-		}
-		else if (date.getMonth() <= 7) {
-			return 'summer';
-		}
-		else if (date.getMonth() <= 10) {
-			return 'autumn';
-		}
-	} else {
-		throw new RangeError('THROW')
-	}
-  throw new CustomError('Not implemented');
-};
+	if (!date) return 'Unable to determine the time of year!'
+	if ( isNaN( date.getTime())) return 'THROWN'
+  
+	if ((date.getMonth() >= 0 && date.getMonth() <= 1) || date.getMonth() === 11) return 'winter'
+	else if (date.getMonth() >= 2 & date.getMonth() <= 4) return 'spring'
+	else if (date.getMonth() >= 5 & date.getMonth() <= 7) return 'summer'
+	else return 'autumn'
+  };
