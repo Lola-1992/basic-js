822f3659226ea1bef58948f014325898b1746851
Olga <70957483+Lola-1992@users.noreply.github.com>
2020-10-21 22:01:21 +0300

    feat:
    feat: add function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(members) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let abc = ''
+  let def = ''
+  
+  if (!members) return false;
+  for (let i = 0; i <= members.length; i++) {
+    if (typeof(members[i]) === 'string') {
+      abc = members[i].replace(/ /g, '').toUpperCase()
+      def += abc[0]
+    }
+  }
+  return def.split('').sort().join('')
 };
